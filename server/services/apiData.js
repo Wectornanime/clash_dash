e047b3f66a5e68125a50df7c76bfc28d224ba66b
Wectornanime <wectornanime@gmail.com>
2024-10-02 21:37:59 -0300

    add sync method

--- server/services/apiData.js
+++ server/services/apiData.js
@@ -3,35 +3,55 @@ const Battle = require('../models/Battle');
 
 // Função para salvar dados da API no MongoDB
 async function saveBattlehData() {
-	try {
-		// Fazendo uma requisição à API
-		const response = await axios.get(`${process.env.CLASH_API_URL}/players/%232RVQG28R2/battlelog`, {
-			headers: {
-				Authorization: `Bearer ${process.env.CLASH_API_TOKEN}`
-			}
-		});
+  try {
+    // Fazendo uma requisição à API
+    const response = await axios.get(`${process.env.CLASH_API_URL}/players/%232RVQG28R2/battlelog`, {
+      headers: {
+        Authorization: `Bearer ${process.env.CLASH_API_TOKEN}`
+      }
+    });
 
-		// Supondo que response.data contenha os dados das partidas
-		const matches = response.data;
+    // Supondo que response.data contenha os dados das partidas
+    const battles = response.data;
 
-    console.log(matches)
+    // Iterando sobre as partidas e salvando no MongoDB
+    for (let battle of battles) {
+      const newBattle = new Battle({
+        player1: {
+          name: battle.team[0].name,
+          playerTag: battle.team[0].tag,
+          startingTrophies: battle.team[0].startingTrophies,
+          cards: battle.team[0].cards.map(item => {
+            return {
+              name: item.name,
+              id: item.id,
+              iconUrl: item.iconUrls.medium || ''
+            }
+          })
+        },
+        player2: {
+          name: battle.opponent[0].name,
+          playerTag: battle.opponent[0].tag,
+          startingTrophies: battle.opponent[0].startingTrophies,
+          cards: battle.opponent[0].cards.map(item => {
+            return {
+              name: item.name,
+              id: item.id,
+              iconUrl: item.iconUrls.medium || ''
+            }
+          })
+        },
+        p1_crowns: battle.team[0].crowns,
+        p2_crowns: battle.opponent[0].crowns,
+        date: battle.battleTime
+      });
 
-		// Iterando sobre as partidas e salvando no MongoDB
-		// for (let match of matches) {
-		// 	const newMatch = new Match({
-		// 		player1: match.player1,
-		// 		player2: match.player2,
-		// 		p1_crowns: match.p1_crowns,
-		// 		p2_crowns: match.p2_crowns,
-		// 		date: match.date // ou algum outro campo para data
-		// 	});
-
-		// 	await newMatch.save();
-		// 	console.log(`Partida entre ${match.player1} e ${match.player2} salva com sucesso!`);
-		// }
-	} catch (error) {
-		console.error('Erro ao salvar dados da API:', error);
-	}
+      await newBattle.save();
+      // console.log(`Partida entre ${match.player1} e ${match.player2} salva com sucesso!`);
+    }
+  } catch (error) {
+    console.error('Erro ao salvar dados da API:', error);
+  }
 };
 
-module.exports = {saveBattlehData}
+module.exports = { saveBattlehData }

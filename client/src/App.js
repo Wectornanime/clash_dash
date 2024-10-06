2ea371f9c3c9decd407edc368d335edb88e7c713
Wectornanime <wectornanime@gmail.com>
2024-10-06 09:54:29 -0300

    update codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import Card from './components/card';
 
 function App() {
   const [inputValue, setInputValue] = useState('');
@@ -18,6 +19,29 @@ function App() {
       window.alert('Ocorreu um erro.');
     }
   };
+
+  const cardData = [
+    {
+      title: "Porcentagem de derrota",
+      apiUrl: "/api/porcentagem",
+      inputs: [
+        { name: "cardName", type: "text", placeholder: "Nome da carta" },
+        { name: "startTime", type: "date", placeholder: "Inicio" },
+        { name: "endTime", type: "date", placeholder: "Fim" }
+      ],
+    },
+    {
+      title: "Card 2",
+      apiUrl: "https://sua-api.com/endpoint2",
+      inputs: [
+        { name: "input1", type: "text", placeholder: "Título" },
+        { name: "input2", type: "date", placeholder: "Data" },
+        { name: "input3", type: "number", placeholder: "Quantidade" },
+      ],
+    },
+    // Adicione mais cards conforme necessário
+  ];
+
   return (
     <div className="App">
       <header>
@@ -33,6 +57,11 @@ function App() {
           <button onClick={handleSubmit}>Importar</button>
         </div>
       </header>
+      <div className='dashboard'>
+        {cardData.map((card, index) => (
+          <Card key={index} apiUrl={card.apiUrl} title={card.title} inputs={card.inputs} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ client/src/components/card.js
@@ -0,0 +1,53 @@
+import './card.css';
+
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Card = ({ apiUrl, title, inputs }) => {
+    const [inputValues, setInputValues] = useState({});
+    const [result, setResult] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setInputValues(prevState => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+    const handleSubmit = async () => {
+        console.log(inputValues)
+        try {
+            const response = await axios.get(apiUrl, inputValues);
+            console.log(response)
+            setResult(response.data); // Ajuste conforme a estrutura da resposta
+        } catch (error) {
+            console.error('Erro ao fazer a requisição:', error);
+            setResult('Ocorreu um erro.');
+        }
+    };
+
+    return (
+        <div style={{ border: '1px solid #ccc', padding: '20px', margin: '10px' }}>
+            <h3>{title}</h3>
+            <div className='inputs'>
+                {inputs.map((input, index) => (
+                    <input
+                        key={index}
+                        type={input.type} // Tipo do input definido pela prop
+                        name={input.name}
+                        placeholder={input.placeholder}
+                        onChange={handleInputChange}
+                    />
+                ))}
+            </div>
+            <button onClick={handleSubmit}>Enviar</button>
+            <div>
+                <h4>Resultado:</h4>
+                <p>{result}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;

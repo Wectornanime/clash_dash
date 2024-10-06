5b34552f227d81ad8793bfc20973c8e24e7728ef
Wectornanime <wectornanime@gmail.com>
2024-10-05 23:16:18 -0300

    init client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('/api/battleApi', { playerTag: inputValue });
+      window.alert(response.data.message); // Supondo que a resposta tenha uma propriedade 'message'
+    } catch (error) {
+      console.error('Erro ao fazer a requisição:', error);
+      window.alert('Ocorreu um erro.');
+    }
+  };
   return (
     <div className="App">
       <header>
         <p>Importar dados: </p>
         <div className='import'>
-          <input type='text' placeholder='Cod. do jogador' width={100} />
-          <button>Importar</button>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder='Cod. do jogador'
+            width={100}
+          />
+          <button onClick={handleSubmit}>Importar</button>
         </div>
       </header>
     </div>
